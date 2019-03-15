210e2481cf8b780e8bfc6f2f744a0662be6f19c9
Bharathi Srinivasan <bharathi.srini@gmail.com>
2019-03-15 10:33:13 +0100

    making features script pythonic

--- BNN_Rec_Sys/feature_engineering.py
+++ BNN_Rec_Sys/feature_engineering.py
@@ -1,82 +1,88 @@
 import pandas as pd
 import gc
 
-# Change folder to location of output of create_data.py
-folder = '/Users/BharathiSrinivasan/Documents/GitHub/Thesis/'
-df = pd.read_csv(folder + 'merged_data.csv')
 
-################ Product Features ############################################################
+def prod_features(df):
+	"""
+	Product Features
+	"""
+	sub1 = df.sort_values(['product_id'],ascending=True)
 
-sub1 = df.sort_values(['product_id'],ascending=True)
+	# 'product_id_orders' indicates the popularity of a product
+	sub2 = sub1.join(sub1.groupby('product_id')['product_id'].size(), on='product_id', rsuffix='_orders')
 
-# 'product_id_orders' indicates the popularity of a product
-sub2 = sub1.join(sub1.groupby('product_id')['product_id'].size(), on='product_id', rsuffix='_orders')
+	# 'reordered_total' indicates how many times a product was reordered
+	sub3 = sub2.join(sub2.groupby('product_id')['reordered'].sum(), on='product_id', rsuffix='_total')
 
-# 'reordered_total' indicates how many times a product was reordered
-sub3 = sub2.join(sub2.groupby('product_id')['reordered'].sum(), on='product_id', rsuffix='_total')
+	del sub1,sub2
+	gc.collect()
 
-prd = sub3[['user_id','product_id', 'product_id_orders','reordered_total']]
+	return sub3[['user_id','product_id', 'product_id_orders','reordered_total']]
 
-del sub1,sub2,sub3
-gc.collect()
 
-print('Product related features created!')
+def user_features(df):
+	"""
+	User features from df dataframe
+	returns users features by id
+	"""
+	# Total orders by user
+	users = df.groupby(['user_id'])['order_number'].max().to_frame('user_orders')
 
-############# User Features ##################################################################
+	# Not ideal - but an indidcation of customer's history with the business
+	users['user_period'] = df.groupby(['user_id'])['days_since_prior_order'].sum()
 
-# Total orders by user
-users = df.groupby(['user_id'])['order_number'].max().to_frame('user_orders')
+	# Average time taken by a user before returning for a purchase
+	users['avg_time_to_order'] = df.groupby(['user_id'])['days_since_prior_order'].mean()
 
-# Not ideal - but an indidcation of customer's history with the business
-users['user_period'] = df.groupby(['user_id'])['days_since_prior_order'].sum()
+	# Total unique products ordered by a customer
+	users['user_distinct_products'] = df.groupby(['user_id'])['product_id'].nunique()
+	 
+	# Total products ordered by a customer
+	users['total_products'] = df.groupby(['user_id'])['product_id'].size()
 
-# Average time taken by a user before returning for a purchase
-users['avg_time_to_order'] = df.groupby(['user_id'])['days_since_prior_order'].mean()
+	# the average basket size of the user
+	users['user_average_basket'] = users['total_products'] / users['user_orders']
 
-# Total unique products ordered by a customer
-users['user_distinct_products'] = df.groupby(['user_id'])['product_id'].nunique()
- 
-# Total products ordered by a customer
-users['total_products'] = df.groupby(['user_id'])['product_id'].size()
+	return users
 
-# the average basket size of the user
-users['user_average_basket'] = users['total_products'] / users['user_orders']
 
-print('User related features created!')
+def interaction_features(df):
+	"""
+	User-Product Features
+	Input: dataframe
+	Outut: features columns
+	"""
 
-########### User-Product Features ###########################################################
+	# Product Reorder Frequency indicates a user's preference for a particular product
+	df['prd_reorder_freq'] = df[df['reordered']==1].groupby(['user_id', 'product_id']).cumcount()+1
 
-# Product Reorder Frequency indicates a user's preference for a particular product
-df['prd_reorder_freq'] = df[df['reordered']==1].groupby(['user_id', 'product_id']).cumcount()+1
+	# Average position of the product in a user's cart
+	df1 = df.groupby(['user_id', 'product_id'])['add_to_cart_order'].mean().to_frame('average_cart_position')
 
-# Average position of the product in a user's cart
-df1 = df.groupby(['user_id', 'product_id'])['add_to_cart_order'].mean().to_frame('average_cart_position')
+	return df1
 
-print('User-Product Features created!')
-########### Merge all features to create final dataframe ####################################
 
-df1 = df1.reset_index()
-df2 = pd.merge(df, df1, how='left', on=['user_id', 'product_id'])
+def create_all(df):
+	"""
+	Merge all features to create final dataframe
+	"""
 
-users = users.reset_index()
-df3 = pd.merge(df2, users, how='left', on='user_id')
+	df1 = interaction_features(df).reset_index()
+	df2 = pd.merge(df, df1, how='left', on=['user_id', 'product_id'])
 
-prd = prd.reset_index()
-df_final = pd.merge(df3, prd, how ='left', on='product_id')
-print('Merged new features into dataset')
+	users = users_features(df).reset_index()
+	df3 = pd.merge(df2, users, how='left', on='user_id')
 
-df_final.drop(['Unnamed: 0', 'user_id_x','index_x','level_0','index_y','user_id_y'], axis=1, inplace=True)
+	prd = prod_features(df).reset_index()
+	df_final = pd.merge(df3, prd, how ='left', on='product_id')
+	print('Merged new features into dataset')
 
-# Export dataset for future use
+	df_final.drop(['Unnamed: 0', 'user_id_x','index_x','level_0','index_y','user_id_y'], axis=1, inplace=True)
 
-outfile = open(folder+'final_data.csv', 'w')
-df_final.to_csv('final_data.csv',index = False, header = True, sep = ',', encoding = 'utf-8')
-outfile.close()
+	del df,df1,df2,df3,users,prd
+	gc.collect()
 
-print('Final dataset writing to csv')
-
-del df,df1,df2,df3,users,prd
-gc.collect()
+	return df_final
 
 
 
