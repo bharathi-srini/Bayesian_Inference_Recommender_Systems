2790e4d8a0116f8d7cbad38bc32d328309f4a360
Bharathi Srinivasan <bharathi.srini@gmail.com>
2019-03-26 22:13:08 +0100

    indexing embedding column function

---
+++ BNN_Rec_Sys/utils.py
@@ -0,0 +1,20 @@
+def val2idx(df, cols):
+	"""
+	Helper to index values of embedding columns
+	"""
+    val_types = dict()
+    for c in cols:
+        val_types[c] = df[c].unique()
+
+    val_to_idx = dict()
+    for k, v in val_types.items():
+        val_to_idx[k] = {o: i for i, o in enumerate(val_types[k])}
+
+    for k, v in val_to_idx.items():
+        df[k] = df[k].apply(lambda x: v[x]+1)
+
+    unique_vals = dict()
+    for c in cols:
+        unique_vals[c] = df[c].nunique()
+
+    return df, unique_vals
